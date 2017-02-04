4084405400892f387cec484efecf5c1041ed61a6
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-02-05 02:38:52 +0300

    tic-tac-toe final version

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,55 @@
 class TicTacToe {
-    constructor() {
+	constructor() {
+		this.count = 0;
+		this.board = [[0,0,0],
+		              [0,0,0],
+		              [0,0,0]];
+	}
 
-    }
+	getCurrentPlayerSymbol() {
+		return this.count % 2 ? 'o' : 'x';
+	}
 
-    getCurrentPlayerSymbol() {
+	nextTurn(rowIndex, columnIndex) {
+		if(!this.board[rowIndex][columnIndex] && this.count < 9) {
+			this.board[rowIndex][columnIndex] = this.count % 2 ?  'o' : 'x';
+			this.count++;
+		}
+	}
 
-    }
+	isFinished() {
+		return (!!this.getWinner() || this.noMoreTurns());
+	}
 
-    nextTurn(rowIndex, columnIndex) {
+	getWinner() {
+		if((this.board[0][0] !=0 && this.board[0][0] == this.board[1][1] && this.board[1][1] == this.board[2][2])
+			|| (this.board[0][2] !=0 && this.board[0][2] == this.board[1][1] && this.board[1][1] == this.board[2][0])) 
+			return this.board[1][1]; 
 
-    }
+		for(var i = 0; i <3; i++) {
+			if(this.board[i][0] !=0 && this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) 
+				return this.board[i][0]
+		}
 
-    isFinished() {
+		for(var i = 0; i < 3; i++){ 
+			if(this.board[0][i] !=0 && this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i])
+				return this.board[0][i]; 
+		}
+		return null;
+	}
 
-    }
+	noMoreTurns() {
+		return this.count == 9 ? true : false;
+	}
 
-    getWinner() {
+	isDraw() {
+		return (this.isFinished() && !this.getWinner());
+	}
 
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	getFieldValue(rowIndex, colIndex) {
+		return this.board[rowIndex][colIndex] ? this.board[rowIndex][colIndex] : null;
+	}
 }
 
 module.exports = TicTacToe;
+
